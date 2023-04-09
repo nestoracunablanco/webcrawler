c5a0e82e820018828ae898d2b4a52a3d7983703c
Nestor Acuna-Blanco <nacuna85@gmail.com>
2023-04-09 15:23:37 +0200

    chore: weekly update
    - jvm-1.8.20
    - shadow-8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "8.1.0"
+    kotlin("jvm") version "1.8.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
